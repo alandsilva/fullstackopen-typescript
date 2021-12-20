51aac2126d5c699acf4fe2634c719ead1516c175
Alan <dasilva.alan@outlook.com>
2021-12-20 20:43:18 +0200

    9.3

--- part1/exerciseCalculator.ts
+++ part1/exerciseCalculator.ts
@@ -7,6 +7,27 @@ interface Result {
   target: number;
   average: number;
 }
+
+interface ExerciseValues {
+  value1: Array<number>;
+  value2: number;
+}
+
+const parseExerciseArguments = (args: Array<string>): ExerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  const arr = args.slice(3).map((num) => Number(num));
+
+  if (!isNaN(Number(args[2])) && !arr.includes(NaN)) {
+    return {
+      value1: arr,
+      value2: Number(args[2]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
 const calculateExercises = (days: Array<number>, target: number): Result => {
   const periodLength = days.length;
   const trainingDays = days.filter((d) => d > 0).length;
@@ -28,4 +49,15 @@ const calculateExercises = (days: Array<number>, target: number): Result => {
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+// console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+
+try {
+  const { value1, value2 } = parseExerciseArguments(process.argv);
+  console.log(calculateExercises(value1, value2));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
