9bdbb2eb9538064cef3f19050a433b0e2d9469a2
Alan <dasilva.alan@outlook.com>
2021-12-26 19:41:44 +0200

    9.14

---
+++ courseinfo/src/components/Total.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface TotalProps {
+  total: number;
+}
+
+const Total = ({ total }: TotalProps) => {
+  return <div>Number of exercises {total}</div>;
+};
+
+export default Total;
