96dbc0ee724c01f3702338a68b28c67eb0b3fee4
Alan <dasilva.alan@outlook.com>
2022-01-22 07:17:03 +0200

    9.19

--- patientor-backend/src/services/patientsService.ts
+++ patientor-backend/src/services/patientsService.ts
@@ -1,10 +1,10 @@
-import patientsData from '../../data/patients.json';
+import patientsData from '../../data/patients';
 
 import { PublicPatient, NewPatient, Patient } from '../types';
 
 import { v1 as uuid } from 'uuid';
 
-const patients: Array<Patient> = patientsData as Array<Patient>;
+const patients: Array<Patient> = patientsData ;
 
 const getEntries = (): PublicPatient[] => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
@@ -20,7 +20,6 @@ const getPatient = (id: string): Patient | undefined => {
   return patients.find((patient) => patient.id === id);
 };
 const addPatient = (patient: NewPatient): PublicPatient => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   const id: string = uuid();
   const newPatient = {
     id: id,
